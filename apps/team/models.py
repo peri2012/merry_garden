4b0037947e134b4c76d5aba06eebbb098f45ad0f
peri2012 <matismailovaaiperi@gmail.com>
2025-08-30 14:01:37 +0300

    2 commit

--- apps/team/models.py
+++ apps/team/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-class Workers:(models.Model)
+class Workers(models.Model):
+    fullname = models.CharField(max_length=255,verbose_name="Ф.И.О" )
+    discription = models.CharField(max_length=)
